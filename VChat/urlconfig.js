2fcbaff6deef0a852636d1e3b5e15698dd30ec4f
Szy0127 <1799853523@qq.com>
2022-07-13 14:18:08 +0800

    feat: use server ip
    Signed-off-by: Szy0127 <1799853523@qq.com>

--- VChat/urlconfig.js
+++ VChat/urlconfig.js
@@ -2,7 +2,7 @@
 
 //后端
 
-const server_ip = '192.168.60.174';
+const server_ip = '119.3.127.254';
 const apiUrl=`http://${server_ip}:8080`;
 const nodeServerUrl = `http://${server_ip}:5000`;
 export {apiUrl,nodeServerUrl};
