2b524b1ae44d29d561dcb7387fe3af61b2afd501
Szy0127 <1799853523@qq.com>
2022-09-09 16:39:09 +0800

    feat: settings

--- VChat/configs/constantConfig.js
+++ VChat/configs/constantConfig.js
@@ -1,3 +1,4 @@
 export const wait_for = 20000;//ms
 export const nonce = "12345";
 export const app_name = "VChat";
+export const server_url = "http://119.3.127.254:3000"
--- VChat/screens/SettingScreen.js
+++ VChat/screens/SettingScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { View,Text,ImageBackground } from "react-native";
 import backgroundImg from "../asserts/background-vertical.png";
+import { server_url } from "../configs/constantConfig";
 export const SettingScreen =()=>{
     return (
         <ImageBackground source={backgroundImg} style={{        flex:1,
             width:'100%',
             height:'100%',
-            position:'absolute'}}>
-            <Text>setting</Text>
+            position:'absolute',
+            flexDirection:"column",
+            alignItems:"center",
+            // justifyContent:"space-around"
+            }}>
+            <Text style={{fontSize:25,marginTop:150,color:"#000000"}}>完整功能请使用电脑浏览器登录网站</Text>
+            <Text style={{fontSize:25,color:"#0000ff"}}>{server_url}</Text>
         </ImageBackground>
     )
 }
